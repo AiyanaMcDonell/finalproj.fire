715b55eb7263fc062490de666de68a4bbd84e5ae
AiyanaMcDonell <amcdonel@hamilton.edu>
2024-10-18 20:15:38 +0000

    started script

---
+++ finalprojscript.R
@@ -0,0 +1,11 @@
+##Script for my final project on wildfires and CO2 emissions in the United States:
+##note: reflect the cumulative materials covered in class.
+
+##article link: https://ourworldindata.org/wildfires
+
+#read in data:
+emissions <- read.csv("/cloud/project/annual-carbon-dioxide-emissions.csv")
+
+#next steps:
+#find spatial data, edit emissions df to only include united states, plot emissions x wildfires, 
+#flags?, visualize data, make predictions, forecast observations
